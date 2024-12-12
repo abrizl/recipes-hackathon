417191f38f8534738d95b8ba127b12f2a79f929c
9thIvy <mythril-pops@protonmail.com>
2024-12-12 13:38:31 -0800

    TAGS!!!!

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,22 @@
 import Header from "../../components/Header/Header";
 import RecipeArea from "../../components/RecipeArea/RecipeArea";
 import Filters from "../../components/Filters/Filters";
+import { useState } from 'react';
 
-function HomePage () {
-    return (
-        <>
-        <Header/>
-        <Filters/>
-        <RecipeArea/>
-        </>
-    );
+function HomePage() {
+  const [selectedTag, setSelectedTag] = useState("");
+
+  function handleTagClick(clickedTag) {
+    setSelectedTag(clickedTag);
+  }
+
+  return (
+    <>
+      <Header />
+      <Filters handleTagClick={handleTagClick} />
+      <RecipeArea selectedTag={selectedTag} /> 
+    </>
+  );
 }
+
 export default HomePage;
