5fc500a1031e5c7ded970a23992bb30ee0c7a409
Umbreen Arshad <umbreenvirk@gmail.com>
2024-12-12 11:00:15 -0800

    add props

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,16 +1,18 @@
 import "./RecipeCard.scss";
 
-function RecipeCard () {
+function RecipeCard ({meal}) {
     return (
         <>
             <section className="recipe-card">
                 <div className="recipe-card__data">
-                    <div className="recipe-card__image"></div>
-                    <div className="recipe-card__information">
-                    <p className="recipe_card__title">RECIPE NAME</p>
-                    <P className="recipe-card__origin">ORIGIN</P>
+                    <div className="recipe-card__top">
+                        <div className="recipe-card__image"></div>
+                        <div className="recipe-card__information">
+                        <p className="recipe_card__title">{meal.strMeal}</p>
+                        <P className="recipe-card__origin">{meal.strArea}</P>
+                        </div>
                     </div>
-                    <div className="recipe-card__recipe">INSTRUCTIONS DATA</div>
+                    <div className="recipe-card__recipe">{meal.strInstructions}</div>
                 </div>
             </section>
         </>
