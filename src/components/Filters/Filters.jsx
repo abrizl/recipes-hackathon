f0eef3d72ef8b9929396246df3ae4d70aa94f786
Umbreen Arshad <umbreenvirk@gmail.com>
2024-12-12 12:00:00 -0800

    filter information added

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,12 +1,26 @@
-// import recipeApi from "../../scripts/api.js";
+import ".Filters.scss";
 
-function Filters(recipeApi) {
+function Filters() {
   return (
-    <section>
-      <h2 className="filters__title">Filters</h2>
-      <ul className="filters__list"></ul>
+    <section className="filters2">
+      <h2 className="filters2__title">Filters</h2>
+      <ul className="filters2__list">
+        <li className="filters2__item">Chicken</li>
+        <li className="filters2__item">Beef</li>
+        <li className="filters2__item">Dessert</li>
+        <li className="filters2__item">Lamb</li>
+        <li className="filters2__item">Miscellaneous</li>
+        <li className="filters2__item">Pasta</li>
+        <li className="filters2__item">Pork</li>
+        <li className="filters2__item">Seafood</li>
+        <li className="filters2__item">Side</li>
+        <li className="filters2__item">Starter</li>
+        <li className="filters2__item">Vegan</li>
+        <li className="filters2__item">Vegetarian</li>
+        <li className="filters2__item">Breakfast</li>
+        <li className="filters2__item">Goat</li>
+      </ul>
     </section>
   );
 }
-
 export default Filters;
