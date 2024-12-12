46b6aa9aa04be4affcf744cfc8ace9532fc83878
Frankie <frankie.m.18@hotmail.com>
2024-12-12 10:41:29 -0800

    filters2

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,27 +1,13 @@
-<<<<<<< Updated upstream
 import Header from "../../components/Header/Header";
 import RecipeArea from "../../components/RecipeArea/RecipeArea";
 
-function HomePage () {
-    return (
-        <>
-        <Header/>
-        <RecipeArea/>
-        </>
-    );
-}
-
-export default HomePage;
-=======
-import Filters from "../../components/Filters/Filters";
-
 function HomePage() {
   return (
-    <div>
-      <Filters />
-    </div>
+    <>
+      <Header />
+      <RecipeArea />
+    </>
   );
 }
 
 export default HomePage;
->>>>>>> Stashed changes
