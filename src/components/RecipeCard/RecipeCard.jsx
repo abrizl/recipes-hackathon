894adb1a2edfde6121b920fafeb419227c27b7c4
Umbreen Arshad <umbreenvirk@gmail.com>
2024-12-12 10:36:33 -0800

    recipecard initalized

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -2,9 +2,18 @@ import "./RecipeCard.scss";
 
 function RecipeCard () {
     return (
-        <section className="recipe-card">
-
-        </section>
+        <>
+            <section className="recipe-card">
+                <div className="recipe-card__data">
+                    <div className="recipe-card__image"></div>
+                    <div className="recipe-card__information">
+                    <p className="recipe_card__title">RECIPE NAME</p>
+                    <P className="recipe-card__origin">ORIGIN</P>
+                    </div>
+                    <div className="recipe-card__recipe">INSTRUCTIONS DATA</div>
+                </div>
+            </section>
+        </>
     );
 }
 
